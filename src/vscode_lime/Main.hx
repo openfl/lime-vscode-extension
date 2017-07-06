8dbba42c2a8f4fb6b858a8658ff35743b6d19b54
Joshua Granick <jgranick@users.noreply.github.com>
2017-07-05 17:56:00 -0700

    Cleanup

--- src/vscode_lime/Main.hx
+++ src/vscode_lime/Main.hx
@@ -1,299 +1,372 @@
 package vscode_lime;
 
+
 import js.node.Buffer;
 import js.node.ChildProcess;
 import Vscode.*;
 import vscode.*;
 
+
 class Main {
+	
+	
+	private var buildConfigItems:Array<BuildConfigItem>;
+	private var context:ExtensionContext;
+	private var displayArguments:Array<String> = [];
+	private var editTargetFlagsItem:StatusBarItem;
+	private var haxeServerAPI:Dynamic;
+	private var haxeServerReady:Bool;
+	private var lastLanguage:String;
+	private var selectBuildConfigItem:StatusBarItem;
+	private var selectTargetItem:StatusBarItem;
+	private var targetItems:Array<TargetItem>;
+	
+	
+	public function new (context:ExtensionContext) {
+		
+		this.context = context;
+		
+		//trace ("Hello from Lime extension");
+		
+		initialize ();
+		construct ();
+		
+	}
+	
+	
+	private function construct ():Void {
+		
+		selectTargetItem = window.createStatusBarItem (Left, 19);
+		selectTargetItem.tooltip = "Select Target";
+		selectTargetItem.command = "lime.selectTarget";
+		context.subscriptions.push (selectTargetItem);
+		
+		selectBuildConfigItem = window.createStatusBarItem (Left, 18);
+		selectBuildConfigItem.tooltip = "Select Build Configuration";
+		selectBuildConfigItem.command = "lime.selectBuildConfig";
+		context.subscriptions.push (selectBuildConfigItem);
+		
+		editTargetFlagsItem = window.createStatusBarItem (Left, 17);
+		editTargetFlagsItem.tooltip = "Additional Command-Line Arguments";
+		editTargetFlagsItem.command = "lime.editTargetFlags";
+		context.subscriptions.push (editTargetFlagsItem);
+		
+		context.subscriptions.push (commands.registerCommand ("lime.selectTarget", selectTargetItem_onCommand));
+		context.subscriptions.push (commands.registerCommand ("lime.selectBuildConfig", selectBuildConfigItem_onCommand));
+		context.subscriptions.push (commands.registerCommand ("lime.editTargetFlags", editTargetFlagsItem_onCommand));
+		
+		context.subscriptions.push (workspace.onDidChangeConfiguration (workspace_onDidChangeConfiguration));
+		context.subscriptions.push (window.onDidChangeActiveTextEditor (window_onDidChangeActiveTextEditor));
+		
+		// TODO: Improve server API
+		
+		var vshaxe = extensions.getExtension("nadako.vshaxe");
+		haxeServerAPI = vshaxe.exports;
 
-    var context:ExtensionContext;
-    var selectTargetItem:StatusBarItem;
-    var selectBuildConfigItem:StatusBarItem;
-    var editTargetFlagsItem:StatusBarItem;
-    var targetItems:Array<TargetItem>;
-    var buildConfigItems:Array<BuildConfigItem>;
-    var lastLanguage:String;
-    var displayArguments:Array<String> = [];
-    var serverReady:Bool;
-    var serverAPI:Dynamic;
+		if (untyped !haxeServerAPI) trace ("Haxe server API not available?");
 
-    function new(context:ExtensionContext) {
-        this.context = context;
-        trace ("Hello from Lime extension");
-
-        selectTargetItem = window.createStatusBarItem(Left, 19);
-        selectTargetItem.tooltip = "Select Target";
-        selectTargetItem.command = "haxe.lime.selectTarget";
-        context.subscriptions.push(selectTargetItem);
-
-        selectBuildConfigItem = window.createStatusBarItem(Left, 18);
-        selectBuildConfigItem.tooltip = "Select Build Configuration";
-        selectBuildConfigItem.command = "haxe.lime.selectBuildConfig";
-        context.subscriptions.push(selectBuildConfigItem);
-
-        editTargetFlagsItem = window.createStatusBarItem(Left, 17);
-        editTargetFlagsItem.tooltip = "Additional Command-Line Arguments";
-        editTargetFlagsItem.command = "haxe.lime.editTargetFlags";
-        context.subscriptions.push(editTargetFlagsItem);
-
-        targetItems = [
-            {
-                target: "windows",
-                label: "Windows",
-                description: "",
-            },
-            {
-                target: "mac",
-                label: "macOS",
-                description: "",
-            },
-            {
-                target: "linux",
-                label: "Linux",
-                description: "",
-            },
-            {
-                target: "ios",
-                label: "iOS",
-                description: "",
-            },
-            {
-                target: "android",
-                label: "Android",
-                description: "",
-            },
-            {
-                target: "flash",
-                label: "Flash",
-                description: "",
-            },
-            {
-                target: "html5",
-                label: "HTML5",
-                description: "",
-            },
-            {
-                target: "emscripten",
-                label: "Emscripten",
-                description: "",
-            }
-        ];
-
-        buildConfigItems = [
-            {
-                flags: "-debug",
-                label: "Debug",
-                description: "",
-            },
-            {
-                flags: "",
-                label: "Release",
-                description: "",
-            },
-            {
-                flags: "-final",
-                label: "Final",
-                description: "",
-            }
-        ];
-
-        context.subscriptions.push(commands.registerCommand("haxe.lime.selectTarget", selectTarget));
-        context.subscriptions.push(commands.registerCommand("haxe.lime.selectBuildConfig", selectBuildConfig));
-        context.subscriptions.push(commands.registerCommand("haxe.lime.editTargetFlags", editTargetFlags));
-
-
-        context.subscriptions.push(workspace.onDidChangeConfiguration(onDidChangeConfiguration));
-        context.subscriptions.push(window.onDidChangeActiveTextEditor(onDidChangeActiveTextEditor));
-
-        var vshaxe = extensions.getExtension("nadako.vshaxe");
-        serverAPI = vshaxe.exports;
-
-        if (untyped !serverAPI) trace ("vshaxe API not available?");
-
-        if (serverAPI.isReady) {
-            //trace ("isReady");
-            onHaxeServerReady();
-        } else {
-            //trace ("onReady");
-            serverAPI.onReady = onHaxeServerReady;
-        }
-
-        updateStatusBarItems();
-    }
-
-    @:keep
-    @:expose("activate")
-    static function main(context:ExtensionContext) {
-        new Main(context);
-    }
-
-    function onHaxeServerReady() {
-        serverReady = true;
-        updateDisplayArguments();
-    }
-
-    function selectTarget() {
-        var items = targetItems;
-
-        window.showQuickPick(items, {matchOnDescription: true, placeHolder: "Select Target"}).then(function(choice:TargetItem) {
-            if (choice == null || choice.target == getTarget())
-                return;
-            setTarget(choice.target);
-        });
-    }
-
-    function selectBuildConfig() {
-        var items = buildConfigItems;
-
-        window.showQuickPick(items, {matchOnDescription: true, placeHolder: "Select Build Configuration"}).then(function(choice:BuildConfigItem) {
-            // TODO: Read target flags, catch automatically
-            if (choice == null || choice.flags == getBuildConfigFlags())
-                return;
-            setBuildConfigFlags(choice.flags);
-        });
-    }
-
-    function editTargetFlags() {
-        var flags = getTargetFlags();
-        window.showInputBox({ prompt: "Additional Command-Line Arguments", value: flags + " ", valueSelection: [ flags.length + 1, flags.length + 1 ] }).then(function(value:String) {
-            setTargetFlags(StringTools.trim(value));
-        });
-    }
-
-    function onDidChangeConfiguration(_) {
-        //fixIndex();
-        updateStatusBarItems();
-        checkConfigurationChange();
-    }
-
-    function onDidChangeActiveTextEditor(_) {
-        updateStatusBarItems();
-    }
-
-    function updateDisplayArguments() {
-        var buildConfigFlags = getBuildConfigFlags();
-        var targetFlags = getTargetFlags();
-        var commandLine = StringTools.trim("lime display " + getTarget() + (buildConfigFlags != "" ? " " + buildConfigFlags : "") + (targetFlags != "" ? " " + targetFlags : ""));
-        trace ("Running display command: " + commandLine);
-        try {
-            var result:Buffer = ChildProcess.execSync(commandLine, { cwd: workspace.rootPath });
-            var args = result.toString().split("\n");
-            //trace (args);
-            displayArguments = args;
-        } catch (e:Dynamic) {
-            trace ("Error running display command: " + commandLine);
-            trace (e);
-        }
-        onDidChangeDisplayArguments(displayArguments);
-    }
-
-    function updateStatusBarItems() {
-        //var hasEditor = (window.activeTextEditor != null);
-        //var isDocument = hasEditor && languages.match({language: 'haxe', scheme: 'file'}, window.activeTextEditor.document) > 0;
-        //var isRelatedPanel = hasEditor && (window.activeTextEditor.document:Dynamic).scheme != "file" && lastLanguage == "haxe";
-
-        if (true /*&& (isDocument || isRelatedPanel)*/) {
-            var target = getTarget();
-            for (i in 0...targetItems.length) {
-                var item = targetItems[i];
-                if (item.target == target) {
-                    selectTargetItem.text = item.label;
-                    selectTargetItem.show();
-                    break;
-                }
-            }
-
-            var buildConfigFlags = getBuildConfigFlags();
-            for (i in 0...buildConfigItems.length) {
-                var item = buildConfigItems[i];
-                if (item.flags == buildConfigFlags) {
-                    selectBuildConfigItem.text = item.label;
-                    selectBuildConfigItem.show();
-                    break;
-                }
-            }
-
-            editTargetFlagsItem.text = "$(list-unordered)";
-            editTargetFlagsItem.show();
-            lastLanguage = "haxe";
-            return;
-        }
-
-        lastLanguage = null;
-        selectTargetItem.hide();
-        selectBuildConfigItem.hide();
-        editTargetFlagsItem.hide();
-    }
-
-    public function getDisplayArguments():Array<String> {
-        if (displayArguments.length == 0 && serverReady) {
-            updateDisplayArguments();
-        }
-        return displayArguments;
-    }
-
-    public function getTarget():String {
-        return context.workspaceState.get("haxe.lime.target", "html5");
-    }
-
-    function setTarget(target:String) {
-        context.workspaceState.update("haxe.lime.target", target);
-        updateStatusBarItems();
-        updateDisplayArguments();
-        //onDidChangeIndex(index);
-        //checkConfigurationChange();
-    }
-
-    public function getBuildConfigFlags():String {
-        return context.workspaceState.get("haxe.lime.buildConfigFlags", "");
-    }
-
-    function setBuildConfigFlags(flags:String) {
-        context.workspaceState.update("haxe.lime.buildConfigFlags", flags);
-        updateStatusBarItems();
-        updateDisplayArguments();
-        //onDidChangeIndex(index);
-        //checkConfigurationChange();
-    }
-
-    public function getTargetFlags():String {
-        return context.workspaceState.get("haxe.lime.additionalTargetFlags", "");
-    }
-
-    function setTargetFlags(flags:String) {
-        context.workspaceState.update("haxe.lime.additionalTargetFlags", flags);
-        updateDisplayArguments();
-        //updateStatusBarItems();
-        //onDidChangeIndex(index);
-        //checkConfigurationChange();
-    }
-
-    function checkConfigurationChange() {
-        // var newConfiguration = getConfiguration();
-        // if (!newConfiguration.equals(configuration)) {
-        //     onDidChangeDisplayConfiguration(newConfiguration);
-        //     configuration = newConfiguration;
-        // }
-    }
-
-    //public dynamic function onDidChangeIndex(index:Int):Void {}
-
-    //public dynamic function onDidChangeDisplayConfiguration(configuration:Array<String>):Void {}
-
-    function onDidChangeDisplayArguments(args:Array<String>):Void {
-        if (serverReady) {
-            serverAPI.updateDisplayArguments(args);
-        }
-    }
+		if (haxeServerAPI.isReady) {
+			
+			haxeServerAPI_onReady ();
+			
+		} else {
+			
+			haxeServerAPI.onReady = haxeServerAPI_onReady;
+			
+		}
+		
+		updateStatusBarItems ();
+		
+	}
+	
+	
+	public function getBuildConfigFlags ():String {
+		
+		return context.workspaceState.get ("lime.buildConfigFlags", "");
+		
+	}
+	
+	
+	public function getTarget ():String {
+		
+		return context.workspaceState.get ("lime.target", "html5");
+		
+	}
+	
+	
+	public function getTargetFlags ():String {
+		
+		return context.workspaceState.get ("lime.additionalTargetFlags", "");
+		
+	}
+	
+	
+	private function initialize ():Void {
+		
+		targetItems = [
+			{
+				target: "windows",
+				label: "Windows",
+				description: "",
+			},
+			{
+				target: "mac",
+				label: "macOS",
+				description: "",
+			},
+			{
+				target: "linux",
+				label: "Linux",
+				description: "",
+			},
+			{
+				target: "ios",
+				label: "iOS",
+				description: "",
+			},
+			{
+				target: "android",
+				label: "Android",
+				description: "",
+			},
+			{
+				target: "flash",
+				label: "Flash",
+				description: "",
+			},
+			{
+				target: "html5",
+				label: "HTML5",
+				description: "",
+			},
+			{
+				target: "emscripten",
+				label: "Emscripten",
+				description: "",
+			}
+		];
+		
+		buildConfigItems = [
+			{
+				flags: "-debug",
+				label: "Debug",
+				description: "",
+			},
+			{
+				flags: "",
+				label: "Release",
+				description: "",
+			},
+			{
+				flags: "-final",
+				label: "Final",
+				description: "",
+			}
+		];
+		
+	}
+	
+	
+	@:keep @:expose("activate") public static function main (context:ExtensionContext) {
+		
+		new Main (context);
+		
+	}
+	
+	
+	public function setBuildConfigFlags (flags:String):Void {
+		
+		context.workspaceState.update ("lime.buildConfigFlags", flags);
+		updateStatusBarItems ();
+		updateDisplayArguments ();
+		
+	}
+	
+	
+	public function setTarget (target:String):Void {
+		
+		context.workspaceState.update ("lime.target", target);
+		updateStatusBarItems ();
+		updateDisplayArguments ();
+		
+	}
+	
+	
+	public function setTargetFlags (flags:String):Void {
+		
+		context.workspaceState.update ("lime.additionalTargetFlags", flags);
+		updateDisplayArguments ();
+		
+	}
+	
+	
+	private function updateDisplayArguments ():Void {
+		
+		var buildConfigFlags = getBuildConfigFlags ();
+		var targetFlags = getTargetFlags ();
+		var commandLine = StringTools.trim ("lime display " + getTarget () + (buildConfigFlags != "" ? " " + buildConfigFlags : "") + (targetFlags != "" ? " " + targetFlags : ""));
+		
+		//trace ("Running display command: " + commandLine);
+		
+		try {
+			
+			var result:Buffer = ChildProcess.execSync (commandLine, { cwd: workspace.rootPath });
+			var args = result.toString ().split ("\n");
+			//trace (args);
+			displayArguments = args;
+			
+			if (haxeServerReady) {
+				
+				haxeServerAPI.updateDisplayArguments (args);
+				
+			}
+			
+		} catch (e:Dynamic) {
+			
+			trace ("Error running display command: " + commandLine);
+			trace (e);
+			
+		}
+		
+	}
+	
+	
+	private function updateStatusBarItems ():Void {
+		
+		//var hasEditor = (window.activeTextEditor != null);
+		//var isDocument = hasEditor && languages.match({language: 'haxe', scheme: 'file'}, window.activeTextEditor.document) > 0;
+		//var isRelatedPanel = hasEditor && (window.activeTextEditor.document:Dynamic).scheme != "file" && lastLanguage == "haxe";
+		
+		if (true /*&& (isDocument || isRelatedPanel)*/) {
+			
+			var target = getTarget ();
+			
+			for (i in 0...targetItems.length) {
+				
+				var item = targetItems[i];
+				if (item.target == target) {
+					
+					selectTargetItem.text = item.label;
+					selectTargetItem.show ();
+					break;
+					
+				}
+				
+			}
+			
+			var buildConfigFlags = getBuildConfigFlags ();
+			
+			for (i in 0...buildConfigItems.length) {
+				
+				var item = buildConfigItems[i];
+				if (item.flags == buildConfigFlags) {
+					
+					selectBuildConfigItem.text = item.label;
+					selectBuildConfigItem.show ();
+					break;
+					
+				}
+				
+			}
+			
+			editTargetFlagsItem.text = "$(list-unordered)";
+			editTargetFlagsItem.show ();
+			lastLanguage = "haxe";
+			return;
+			
+		}
+		
+		lastLanguage = null;
+		selectTargetItem.hide ();
+		selectBuildConfigItem.hide ();
+		editTargetFlagsItem.hide ();
+		
+	}
+	
+	
+	
+	
+	// Event Handlers
+	
+	
+	
+	
+	private function editTargetFlagsItem_onCommand ():Void {
+		
+		var flags = getTargetFlags ();
+		window.showInputBox ({ prompt: "Additional Command-Line Arguments", value: flags + " ", valueSelection: [ flags.length + 1, flags.length + 1 ] }).then (function (value:String) {
+			
+			setTargetFlags (StringTools.trim (value));
+			
+		});
+		
+	}
+	
+	
+	private function haxeServerAPI_onReady ():Void {
+		
+		haxeServerReady = true;
+		updateDisplayArguments ();
+		
+	}
+	
+	
+	private function selectBuildConfigItem_onCommand ():Void {
+		
+		var items = buildConfigItems;
+		window.showQuickPick (items, { matchOnDescription: true, placeHolder: "Select Build Configuration"}).then (function (choice:BuildConfigItem) {
+			
+			// TODO: Update if target flags include a build configuration?
+			
+			if (choice == null || choice.flags == getBuildConfigFlags ())
+				return;
+				
+			setBuildConfigFlags (choice.flags);
+			
+		});
+		
+	}
+	
+	
+	private function selectTargetItem_onCommand ():Void {
+		
+		var items = targetItems;
+		window.showQuickPick (items, { matchOnDescription: true, placeHolder: "Select Target" }).then (function (choice:TargetItem) {
+			
+			if (choice == null || choice.target == getTarget ())
+				return;
+			
+			setTarget (choice.target);
+			
+		});
+		
+	}
+	
+	
+	private function window_onDidChangeActiveTextEditor (_):Void {
+		
+		updateStatusBarItems ();
+		
+	}
+	
+	
+	private function workspace_onDidChangeConfiguration (_):Void {
+		
+		updateStatusBarItems ();
+		
+	}
+	
+	
 }
+
 
 private typedef TargetItem = {
-    >QuickPickItem,
-    var target:String;
+	>QuickPickItem,
+	var target:String;
 }
 
+
 private typedef BuildConfigItem = {
-    >QuickPickItem,
-    var flags:String;
+	>QuickPickItem,
+	var flags:String;
 }
